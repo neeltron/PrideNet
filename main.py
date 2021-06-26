0ff27e0db07183d403f315f0dc75951384572b4a
Neel Adwani <nprosofficial@gmail.com>
2021-06-26 04:02:35 +0000

    Flask Server Basic

--- main.py
+++ main.py
@@ -1,8 +1,21 @@
-from flask import Flask
-app = Flask('app')
+from flask import Flask, render_template
 
+# Create a flask app
+app = Flask(
+  __name__,
+  template_folder='templates',
+  static_folder='static'
+)
+
+# Index page
 @app.route('/')
-def hello_world():
-  return 'Hello, World!'
+def hello():
+  return render_template('index.html')
 
-app.run(host='0.0.0.0', port=8080)
+if __name__ == '__main__':
+  # Run the Flask app
+  app.run(
+	host='0.0.0.0',
+	debug=True,
+	port=8080
+  )
